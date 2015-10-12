c499c7024cf6354f4bea67eb522402e9f55a4638
Drew Budwin <dbudwin@foxguardsolutions.com>
2015-10-12 11:29:46 -0400

    Monopoly release 1 commit
    -Implements the board and basic player movement
    -Test cases for the provided User Stories
    -Please review my use of Moq for Mocking random numbers
    -Use of Ninject for DI
    -Created factories for making players and board spaces

---
+++ IBoardSpacesFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Monopoly
+{
+    public interface IBoardSpacesFactory
+    {
+        IEnumerable<IBoardSpace> CreateAll();
+    }
+}
---
+++ RandomNumberGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Monopoly
+{
+    public class RandomNumberGenerator : IRandomNumberGenerator
+    {
+        private readonly Random random = new Random();
+
+        public int Generate(int min, int max)
+        {
+            return random.Next(min, max + 1);
+        }
+    }
+}
